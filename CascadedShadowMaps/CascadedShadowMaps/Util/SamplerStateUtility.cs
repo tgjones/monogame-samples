f5d5dc762a50b686bd31541a13c1a42bd9bd021e
Roy Triesscheijn <roy-t@hotmail.com>
2018-03-27 19:54:11 +0200

    Fix issues with MonoGame 3.6

--- CascadedShadowMaps/CascadedShadowMaps/Util/SamplerStateUtility.cs
+++ CascadedShadowMaps/CascadedShadowMaps/Util/SamplerStateUtility.cs
@@ -4,13 +4,14 @@ namespace ShadowsSample.Util
 {
     public static class SamplerStateUtility
     {
-         public static readonly SamplerState ShadowMap = new SamplerState
-         {
-             AddressU = TextureAddressMode.Clamp,
-             AddressV = TextureAddressMode.Clamp,
-             AddressW = TextureAddressMode.Clamp,
-             Filter = TextureFilter.Linear,
-             ComparisonFunction = CompareFunction.LessEqual
-         };
+        public static readonly SamplerState ShadowMap = new SamplerState
+        {
+            AddressU = TextureAddressMode.Clamp,
+            AddressV = TextureAddressMode.Clamp,
+            AddressW = TextureAddressMode.Clamp,
+            Filter = TextureFilter.Linear,
+            ComparisonFunction = CompareFunction.LessEqual,
+            FilterMode = TextureFilterMode.Comparison
+        };
     }
 }
